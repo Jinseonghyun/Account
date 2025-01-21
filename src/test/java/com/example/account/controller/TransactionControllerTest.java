6093b203cae853d92f5e3b517344dee6ac626969
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-22 00:35:38 +0900

    잔액 사용 확인 api 개발 및 테스트 코드

--- src/test/java/com/example/account/controller/TransactionControllerTest.java
+++ src/test/java/com/example/account/controller/TransactionControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.account.controller;
 
+import com.example.account.dto.AccountDto;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.TransactionDto;
 import com.example.account.dto.UseBalance;
 import com.example.account.service.TransactionService;
 import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -16,14 +18,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.example.account.type.TransactionResultType.*;
+import static com.example.account.type.TransactionType.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TransactionController.class)
@@ -89,7 +96,29 @@ class TransactionControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transactionResultType").value("S"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transactionId").value("transactionIdForCancel"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value(54321));
+    }
 
-
+    @Test
+    void successQueryTransaction() throws Exception {
+        //given
+        given(transactionService.queryTransaction(anyString()))
+                .willReturn(TransactionDto.builder()
+                        .accountNumber("1000000000")
+                        .transactionType(USE)
+                        .transactedAt(LocalDateTime.now())
+                        .amount(54321L)
+                        .transactionId("transactionIdForCancel")
+                        .transactionResultType(S)
+                        .build());
+        //when
+        //then
+        mockMvc.perform(get("/transaction/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value("1000000000"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transactionType").value("USE"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transactionResultType").value("S"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transactionId").value("transactionIdForCancel"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value(54321));
     }
 }
